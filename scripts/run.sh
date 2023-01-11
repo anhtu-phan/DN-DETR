6fe611299be0cdc846257372a28ba76b2a08bab9
anhtu95 <anhtu23795@gmail.com>
2023-01-11 10:44:20 +0100

    update dockerfile

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,8 @@ NAME="dn_dab_deformable_detr"
 PROJECT_NAME="chick"
 NUM_CLASSES = 3
 
+cd models/dn_dab_deformable_detr/ops
+python setup.py build install
+cd ../../..
+
 python -m torch.distributed.launch main.py -m $NAME --output_dir logs/$PROJECT_NAME/$NAME --batch_size 1 --epochs 300 --lr_drop 20 --coco_path ~/detr/datasets/$PROJECT_NAME --use_dn --wandb_name $NAME --finetune_ignore class_embed label_enc --pretrain_model_path models/dn_dab_deformable_detr/checkpoint0049.pth --transformer_activation relu --wandb_project_name $PROJECT_NAME --num_classes $NUM_CLASSES
